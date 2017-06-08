659874e50b0ab4d03a19cdd7904fb5ed9a094c0b
pontura <pontura@gmail.com>
2017-06-08 12:13:28 -0300

    Gear VR

--- ciruli/Assets/TV.cs
+++ ciruli/Assets/TV.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class TV : MonoBehaviour {
 	
-	public MovieTexture movTexture;
+	//public MovieTexture movTexture;
 
 	void Start() {
-		GetComponent<Renderer>().material.mainTexture = movTexture;
-		movTexture.Play();
+		//GetComponent<Renderer>().material.mainTexture = movTexture;
+		//movTexture.Play();
 	}
 }
